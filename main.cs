89c2ebf1aa179ddc871d2b21bbde9cc0de0d7f61
JaredYoung1 <143967567+JaredYoung1@users.noreply.github.com>
2023-10-23 00:53:26 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    int x = 1;
+    double y = 2.54;
+
+    double total = x * y;
+
+    Console.WriteLine("{0} * {1} = {2}", x, y, total);
+    
+    
+  }
+}
